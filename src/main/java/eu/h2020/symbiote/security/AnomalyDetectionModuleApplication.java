208950c3d2ccb688756348d15d871c7c9fa28e2d
Mikołaj Dobski <mikolaj.dobski@man.poznan.pl>
2018-06-11 10:46:00 +0200

    code review and cleanup

--- src/main/java/eu/h2020/symbiote/security/AnomalyDetectionModuleApplication.java
+++ src/main/java/eu/h2020/symbiote/security/AnomalyDetectionModuleApplication.java
@@ -5,14 +5,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-import java.security.KeyManagementException;
-import java.security.NoSuchAlgorithmException;
-
 @EnableDiscoveryClient
 @SpringBootApplication(scanBasePackages = "eu.h2020.symbiote.security")
 public class AnomalyDetectionModuleApplication {
 
-    public static void main(String[] args) throws NoSuchAlgorithmException, KeyManagementException {
+    public static void main(String[] args) {
         ECDSAHelper.enableECDSAProvider();
         SpringApplication.run(AnomalyDetectionModuleApplication.class, args);
     }
